bc982b572639c8542f4a5e9b6a8cf9369a56c6f8
ilge.ustun <153606956+ilge-ustun@users.noreply.github.com>
2025-10-18 20:20:36 +0200

    networks

--- src/config/wagmiNetworks.ts
+++ src/config/wagmiNetworks.ts
@@ -1,16 +1,42 @@
-import { arbitrumSepolia } from '@reown/appkit/networks';
+import {
+  arbitrumSepolia,
+  gnosis,
+} from '@reown/appkit/networks';
 
-export { arbitrumSepolia } from '@reown/appkit/networks';
+export { arbitrumSepolia, gnosis } from '@reown/appkit/networks';
+
+// export const bellecour: AppKitNetwork = {
+//   id: 0x86,
+//   name: 'iExec Sidechain',
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: 'xRLC',
+//     symbol: 'xRLC',
+//   },
+//   rpcUrls: {
+//     public: { http: ['https://bellecour.iex.ec'] },
+//     default: { http: ['https://bellecour.iex.ec'] },
+//   },
+//   blockExplorers: {
+//     etherscan: {
+//       name: 'Blockscout',
+//       url: 'https://blockscout-bellecour.iex.ec',
+//     },
+//     default: { name: 'Blockscout', url: 'https://blockscout-bellecour.iex.ec' },
+//   },
+// };
 
 // Explorer slugs mapping for iExec explorer
 export const explorerSlugs: Record<number, string> = {
   134: 'bellecour', // iExec Sidechain (Bellecour)
   42161: 'arbitrum-mainnet', // Arbitrum One
   421614: 'arbitrum-sepolia-testnet', // Arbitrum Sepolia
+  100: 'gnosis-chain', // Gnosis
 };
 
 const wagmiNetworks = {
   arbitrumSepolia,
+  gnosis,
 };
 
 export default wagmiNetworks;
